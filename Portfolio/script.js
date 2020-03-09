676b3eaa4f7b32db85477db4effdd1bcf5f7768b
Josh Brooks <defaultjosh@hotmail.com>
2020-03-09 04:33:08 -0400

    Updated title tags, linked js to contact page

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,1 +1,10 @@
 //Make my contact me email form work properly
+
+//set variable name and tie it to the submit button
+var subBtn = document.getElementsByClassName("submit-btn");
+
+//Create array to store name, email and message
+var emailInfo = []
+
+//create event listener for submit button variable that executes the subit function
+subBtn.addEventListener("click", alert("The click function works... once I make it.") )
