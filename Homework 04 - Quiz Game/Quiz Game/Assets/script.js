922383acb31221a7af40a930ea0a6bfa61e17644
Josh Brooks <defaultjosh@hotmail.com>
2020-03-12 18:24:21 -0400

    commented out all status bar content... for now.

--- Homework 04 - Quiz Game/Quiz Game/Assets/script.js
+++ Homework 04 - Quiz Game/Quiz Game/Assets/script.js
@@ -5,14 +5,14 @@ var bText = document.getElementById("answerB");
 var cText = document.getElementById("answerC");
 var dText = document.getElementById("answerD");
 //variable to control growth of progress bar as questions are answered. Bar controlled by style= width: x%
-var progBar = document.getElementsByClassName("progress-bar");
+// var progBar = document.getElementsByClassName("progress-bar");
 //answer arrays to set output of click events
 var aTrue = [a = true, b = false, c = false, d= false]
 var bTrue = [a = false, b = true, c = false, d= false]
 var cTrue = [a = false, b = false, c = true, d= false]
 var dTrue = [a = false, b = false, c = false, d= true]
 //variable to adjust bar growth
-var barGrow = 0;
+// var barGrow = 0;
 //number of questions variable
 var questNum = 10;
 //outputs to screen
