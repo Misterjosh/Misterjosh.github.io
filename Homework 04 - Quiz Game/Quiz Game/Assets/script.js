bc8abdd795e0d4dc9a99af8056150433e357ea51
Josh Brooks <defaultjosh@hotmail.com>
2020-03-12 16:42:52 -0400

    Added question objects and homework readme

--- Homework 04 - Quiz Game/Quiz Game/Assets/script.js
+++ Homework 04 - Quiz Game/Quiz Game/Assets/script.js
@@ -21,20 +21,21 @@ aText.tectContent;
 bText.tectContent;
 cText.tectContent;
 dText.tectContent;
-//User name from prompt
-var userName = prompt("What is your name?");
-console.log(userName);
-alert("There are 10 questions. Good luck " + userName + "!");
-
-
-
-
-
-
-
-
-
-
-
-
-
+//setting up object to make questions
+function Question(question, answerA, answerB, answerC, answerD) {
+    this.question = question;
+    this.answerA = answerA;
+    this.answerB = answerB;
+    this.answerC = answerC;
+    this.answerD = answerD;
+}
+var question01 = new Question("What is the square root of 4?", '7', '2', "This", '4');
+var question02 = new Question("What is the atomic number of Gold?", "was", '5', '45', '197');
+var question03 = new Question("What does var stand for?", 'variable', 'made', 'German for far', 'September');
+var question04 = new Question("What is the airspeed velocity of an unladen European swallow?", 'by', '10 meters/second', 'The speed of sound', 'The Sound of Music');
+var question05 = new Question("What wouldn't Meatloaf do for love?", 'anything', 'everything', 'that', 'the');
+var question06 = new Question("Do you know what question we are on?", '6', 'one', 'too', 'tree fiddy');
+var question07 = new Question("Who put the bomp In the bomp bah bomp bah bomp?", 'Chuck Berry', 'Barry Mann', 'and', 'Ron Swanson');
+var question08 = new Question('Is it dangerous to go alone?', 'No', 'Yes', 'only', 'Scuba Steve');
+var question09 = new Question('Who framed Rodger Rabbit?', 'Joshua', 'Jessica', 'Christopher Lloyd', 'Bob Haskins');
+var question10 = new Question('What is Garth Brooks last name?', 'Garth', 'Brooks', 'Algar', 'Htrag');
